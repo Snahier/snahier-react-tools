388676b239b2dd50aae0f9fba638373f1c08a0e8
Snahier <hansalmeida@hotmail.com>
2021-01-25 14:28:26 -0300

    Add global style

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components/macro"
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    
+    margin: 0;
+    padding: 0;
+  }
+
+  html,body,#root {
+    height: 100%;
+    font-size: 100%;
+    font-family: "proxima-nova", sans-serif;
+    
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
