80fac0d81c877aea84fbfeef97b8eb82a03d8b37
Snahier <hansalmeida@hotmail.com>
2021-01-25 15:49:29 -0300

    Add scroll snap center

--- src/components/CarouselSlide/styles.ts
+++ src/components/CarouselSlide/styles.ts
@@ -1,5 +1,15 @@
 import styled from "styled-components/macro"
 
 export const StyledCarouselSlide = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 100%;
+
+  scroll-snap-type: x mandatory;
+  overflow-x: scroll;
+  > * {
+    scroll-snap-align: center;
+  }
+
   background-color: lightblue;
 `
