a9ec7fa2654c2f484a0776805695e4a248de35bf
Snahier <hansalmeida@hotmail.com>
2021-01-26 19:34:16 -0300

    Add home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { StyledPageHome } from "./styles"
+
+interface StyledPageHomeProps {}
+
+export const PageHome: React.FC<StyledPageHomeProps> = () => {
+  return (
+    <StyledPageHome>
+      <div>StyledPageHome Component</div>
+    </StyledPageHome>
+  )
+}
--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { PageCarouselSlide } from "./CarouselSlide"
+import { PageHome } from "./Home"
 
 export const Routes = (props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={PageHome} />
         <Route path="/carousel-slide" component={PageCarouselSlide} />
       </Switch>
     </BrowserRouter>
