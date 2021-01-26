f158e50430fe79546a798141a4bfd8a93a7c07e5
Snahier <hansalmeida@hotmail.com>
2021-01-26 19:32:58 -0300

    Add routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-import { CarouselSlide } from "./components/CarouselSlide"
-import { Slide } from "./components/CarouselSlide/Slide"
+import { Routes } from "./pages/routes"
 import { GlobalStyles } from "./styles/GlobalStyles"
 
 function App() {
@@ -8,23 +7,7 @@ function App() {
     <div className="App">
       <GlobalStyles />
       <h1>Components</h1>
-
-      <CarouselSlide
-        buttons={{
-          active: <button>active</button>,
-          inactive: <button>inactive</button>,
-        }}
-      >
-        <Slide>
-          <img src="https://picsum.photos/120?random=1" alt="" />
-        </Slide>
-        <Slide>
-          <img src="https://picsum.photos/120?random=2" alt="" />
-        </Slide>
-        <Slide>
-          <img src="https://picsum.photos/120?random=3" alt="" />
-        </Slide>
-      </CarouselSlide>
+      <Routes />
     </div>
   )
 }
