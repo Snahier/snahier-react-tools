1c78c7c4a0ec17fb6fa53a2ef5ff81b36d3043c1
Snahier <hansalmeida@hotmail.com>
2021-01-26 23:06:50 -0300

    Add component list grid

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components/macro"
+import { viewport } from "../../styles/helpers/viewport"
 
 export const StyledPageHome = styled.div`
   ${({ theme }) => css`
@@ -15,4 +16,16 @@ export const StyledPageHome = styled.div`
   `}
 `
 
-export const ComponentsList = styled.div``
+export const ComponentsList = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: repeat(2, 1fr);
+
+  @media (min-width: ${viewport.tablets}) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: ${viewport.desktop.extraLarge}) {
+    grid-template-columns: repeat(6, 1fr);
+  }
+`
