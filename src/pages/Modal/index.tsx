040a2750ae4c6ebf29e88306788573d5502ee389
Snahier <hansalmeida@hotmail.com>
2021-01-26 23:06:41 -0300

    Create modal page

---
+++ src/pages/Modal/index.tsx
@@ -0,0 +1,11 @@
+import { StyledPageModal } from "./styles"
+
+interface PageModalProps {}
+
+export const PageModal: React.FC<PageModalProps> = () => {
+  return (
+    <StyledPageModal>
+      <div>PageModal Component</div>
+    </StyledPageModal>
+  )
+}
--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { PageCarouselSlide } from "./CarouselSlide"
 import { PageHome } from "./Home"
+import { PageModal } from "./Modal"
 
 export const Routes = (props) => {
   return (
@@ -8,6 +9,7 @@ export const Routes = (props) => {
       <Switch>
         <Route exact path="/" component={PageHome} />
         <Route path="/carousel-slide" component={PageCarouselSlide} />
+        <Route path="/modal" component={PageModal} />
       </Switch>
     </BrowserRouter>
   )
