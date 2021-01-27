089a8cd87aca10a98add9a6237a11b8e12efae2c
Snahier <hansalmeida@hotmail.com>
2021-01-26 23:29:13 -0300

    Refactor values for better naming

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,9 +7,14 @@ interface ModalProps {
 }
 
 export const Modal: React.FC<ModalProps> = ({ opacity = 20, ...props }) => {
+  const opacityPercentageInDecimals = opacity / 100
+
   return props.open ? (
     <StyledModal>
-      <BackgroundBox opacity={opacity / 100} onClick={() => props.onClose()} />
+      <BackgroundBox
+        opacity={opacityPercentageInDecimals}
+        onClick={() => props.onClose()}
+      />
       <div>StyledModal Component</div>
     </StyledModal>
   ) : null
