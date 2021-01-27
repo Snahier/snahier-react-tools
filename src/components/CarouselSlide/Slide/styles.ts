0ab8246d1e0f40abaf159efdbf8f1c0ca3844d5f
Snahier <hansalmeida@hotmail.com>
2021-01-26 22:58:27 -0300

    Make children adjust to width of parent

--- src/components/CarouselSlide/Slide/styles.ts
+++ src/components/CarouselSlide/Slide/styles.ts
@@ -4,4 +4,12 @@ export const StyledSlide = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  width: 100%;
+  max-width: 100%;
+  height: 100%;
+
+  > * {
+    max-width: 100%;
+  }
 `
