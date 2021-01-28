7a94f8decf6059766b4d80f8fbbfec2faa34a9cc
Snahier <hansalmeida@hotmail.com>
2021-01-27 21:46:51 -0300

    Add tooltip content toggle functionality

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,7 +1,31 @@
-import { StyledTooltip } from "./styles"
+import { useState } from "react"
+import {
+  StyledTooltip,
+  TooltipChildWrapper,
+  TooltipContentWrapper,
+} from "./styles"
 
 interface TooltipProps {}
 
 export const Tooltip: React.FC<TooltipProps> = (props) => {
-  return <StyledTooltip>{props.children}</StyledTooltip>
+  const [isActive, setIsActive] = useState(false)
+
+  function toggleTooltip() {
+    setIsActive(!isActive)
+  }
+
+  return (
+    <StyledTooltip>
+      {isActive && (
+        <TooltipContentWrapper>Tooltip content example</TooltipContentWrapper>
+      )}
+
+      <TooltipChildWrapper
+        onMouseEnter={toggleTooltip}
+        onMouseLeave={toggleTooltip}
+      >
+        {props.children}
+      </TooltipChildWrapper>
+    </StyledTooltip>
+  )
 }
--- src/components/Tooltip/styles.ts
+++ src/components/Tooltip/styles.ts
@@ -1,3 +1,7 @@
 import styled from "styled-components/macro"
 
 export const StyledTooltip = styled.div``
+
+export const TooltipChildWrapper = styled.div``
+
+export const TooltipContentWrapper = styled.div``
