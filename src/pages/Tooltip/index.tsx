b8a1886b38e9e1822fd25e19cf778c226dbad9d7
Snahier <hansalmeida@hotmail.com>
2021-01-27 21:12:26 -0300

    Add header

--- src/pages/Tooltip/index.tsx
+++ src/pages/Tooltip/index.tsx
@@ -1,10 +1,12 @@
+import { Header } from "../../components/Header"
 import { StyledTooltip } from "./styles"
 
 interface PageTooltipProps {}
 
 export const PageTooltip: React.FC<PageTooltipProps> = () => {
   return (
-    <StyledTooltip>
+    <StyledTooltip className="page">
+      <Header />
       <div>Tooltip Component</div>
     </StyledTooltip>
   )
