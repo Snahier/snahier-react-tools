d52de9c961f050d249581222a692ad1466d88dd8
Snahier <hansalmeida@hotmail.com>
2021-01-31 05:48:11 -0300

    Add clickMode

--- src/components/PopperTooltip/index.tsx
+++ src/components/PopperTooltip/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react"
+import { ReactNode, useEffect, useRef, useState } from "react"
 import { usePopper } from "react-popper"
 import {
   Arrow,
@@ -11,13 +11,17 @@ interface PopperTooltipProps {
   content: ReactNode
   placement?: "top" | "bottom" | "right" | "left"
   arrow?: boolean
+  clickMode?: boolean
 }
 
 export const PopperTooltip: React.FC<PopperTooltipProps> = ({
   placement = "top",
   arrow,
+  clickMode,
   ...props
 }) => {
+  const componentRef = useRef<HTMLDivElement | null>(null)
+
   const [boxRef, setBoxRef] = useState<HTMLDivElement | null>(null)
   const [tooltipRef, setTooltipRef] = useState<HTMLDivElement | null>(null)
   const [arrowRef, setArrowRef] = useState<HTMLDivElement | null>(null)
@@ -42,11 +46,36 @@ export const PopperTooltip: React.FC<PopperTooltipProps> = ({
     placement,
   })
 
-  const toggleTooltip = () => setIsOpen(!isOpen)
+  const hoverMode = !clickMode
 
+  const toggleTooltip = () => setIsOpen(!isOpen)
+  const activateTooltip = () => setIsOpen(true)
+  const deactivateTooltip = () => setIsOpen(false)
+
+  useEffect(() => {
+    const deactivateOnOutsideClick = (event: MouseEvent) => {
+      !componentRef.current?.contains(event.target as Node) &&
+        deactivateTooltip()
+    }
+
+    clickMode && isOpen
+      ? document.addEventListener("click", deactivateOnOutsideClick)
+      : document.removeEventListener("click", deactivateOnOutsideClick)
+
+    return () => {
+      document.removeEventListener("click", deactivateOnOutsideClick)
+    }
+  }, [isOpen, clickMode])
+
+  console.log({ isOpen })
   return (
-    <StyledPopperTooltip id="popper">
-      <RootElementWrapper ref={setBoxRef} onClick={toggleTooltip}>
+    <StyledPopperTooltip id="popper" ref={componentRef}>
+      <RootElementWrapper
+        ref={setBoxRef}
+        onClick={() => clickMode && toggleTooltip()}
+        onMouseEnter={() => hoverMode && activateTooltip()}
+        onMouseLeave={() => hoverMode && deactivateTooltip()}
+      >
         {props.children}
       </RootElementWrapper>
 
