07fac26505c411eee74ffd505dfd53ef3eacd4a0
Snahier <hansalmeida@hotmail.com>
2021-01-31 22:56:38 -0300

    Add page styling

--- src/pages/Buttons/index.tsx
+++ src/pages/Buttons/index.tsx
@@ -1,11 +1,13 @@
+import { Header } from "../../components/Header"
 import { StyledPageButtons } from "./styles"
 
 interface PageButtonsProps {}
 
 export const PageButtons: React.FC<PageButtonsProps> = () => {
   return (
-    <StyledPageButtons>
-      <div>PageButtons Component</div>
+    <StyledPageButtons className="page">
+      <Header />
+      <h2>Buttons</h2>
     </StyledPageButtons>
   )
 }
