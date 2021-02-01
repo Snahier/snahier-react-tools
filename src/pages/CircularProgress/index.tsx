733aecdd97c2823ce936d4bb563c56fbf3d812e6
Snahier <hansalmeida@hotmail.com>
2021-02-01 01:24:08 -0300

    Add 2 examples of circular progress

--- src/pages/CircularProgress/index.tsx
+++ src/pages/CircularProgress/index.tsx
@@ -1,30 +1,36 @@
 import { useState } from "react"
 import { CircularProgress } from "../../components/CircularProgress"
 import { Header } from "../../components/Header"
-import { StyledPageCircularProgress } from "./styles"
+import { Content, StyledPageCircularProgress } from "./styles"
 
 interface PageCircularProgressProps {}
 
 export const PageCircularProgress: React.FC<PageCircularProgressProps> = () => {
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(50)
 
   return (
     <StyledPageCircularProgress className="page">
       <Header />
       <h2>Circular Progress Component</h2>
 
-      <CircularProgress
-        color="primary"
-        variant="determinate"
-        value={value}
-        size={100}
-      />
-
       <input
         type="text"
         value={value}
         onChange={(event) => setValue(Number(event.target.value))}
+        style={{
+          maxWidth: "20ch",
+        }}
       />
+      <Content>
+        <CircularProgress
+          color="primary"
+          variant="determinate"
+          value={value}
+          size={50}
+        />
+
+        <CircularProgress color="primary" variant="indeterminate" />
+      </Content>
     </StyledPageCircularProgress>
   )
 }
