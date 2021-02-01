7aab172c78c243f08f95ca9512ce7c4fd4156527
Snahier <hansalmeida@hotmail.com>
2021-02-01 02:15:41 -0300

    Add random values to show transition

--- src/pages/CircularProgress/index.tsx
+++ src/pages/CircularProgress/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { CircularProgress } from "../../components/CircularProgress"
 import { Header } from "../../components/Header"
+import { randomIntegerBetween } from "../../helpers/randomIntegerBetween"
 import { Content, StyledPageCircularProgress } from "./styles"
 
 interface PageCircularProgressProps {}
@@ -8,28 +9,38 @@ interface PageCircularProgressProps {}
 export const PageCircularProgress: React.FC<PageCircularProgressProps> = () => {
   const [value, setValue] = useState(50)
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue(randomIntegerBetween(0, 100))
+    }, 3000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [value])
+
   return (
     <StyledPageCircularProgress className="page">
       <Header />
       <h2>Circular Progress Component</h2>
 
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => setValue(Number(event.target.value))}
-        style={{
-          maxWidth: "20ch",
-        }}
-      />
       <Content>
         <CircularProgress
           color="primary"
           variant="determinate"
           value={value}
-          size={50}
+          size={150}
         />
 
-        <CircularProgress color="primary" variant="indeterminate" />
+        <CircularProgress
+          color="primary"
+          variant="determinate"
+          size={150}
+          value={value}
+          round
+        />
+
+        <CircularProgress color="primary" variant="indeterminate" size={100} />
       </Content>
     </StyledPageCircularProgress>
   )
