8927e204d82981f5161cda16d85bb2658314cf05
Snahier <hansalmeida@hotmail.com>
2021-02-10 11:04:00 -0300

    Remove default style, add hover color

--- src/components/core/Button/styles.ts
+++ src/components/core/Button/styles.ts
@@ -1,4 +1,4 @@
-import { readableColor } from "polished"
+import { lighten, readableColor } from "polished"
 import styled, { css } from "styled-components/macro"
 
 interface ButtonProps {
@@ -7,7 +7,15 @@ interface ButtonProps {
 
 export const StyledButton = styled.button<ButtonProps>`
   ${({ color }) => css`
+    padding: 0.5rem;
+
+    border: none;
     background: ${color};
+
     color: ${readableColor(color)};
+
+    &:hover {
+      background: ${lighten(0.1, color)};
+    }
   `}
 `
