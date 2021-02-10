79251c5ff8ddfe35e6747c561e5f5d8eae82027c
Snahier <hansalmeida@hotmail.com>
2021-02-10 13:25:56 -0300

    Add loading property

--- src/components/core/Button/index.tsx
+++ src/components/core/Button/index.tsx
@@ -1,17 +1,27 @@
 import { ButtonHTMLAttributes } from "react"
-import { StyledButton } from "./styles"
+import { CircularProgress } from "../CircularProgress"
+import { LoadWrapper, StyledButton } from "./styles"
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color?: string
+  loading?: boolean
 }
 
 export const Button: React.FC<ButtonProps> = ({
   color = "#00008b",
+  loading,
   children,
   ...props
 }) => {
+  const displayLoader = () => (
+    <LoadWrapper color={color}>
+      <CircularProgress size="1.5em" />
+    </LoadWrapper>
+  )
+
   return (
     <StyledButton color={color} {...props}>
+      {loading && displayLoader()}
       {children}
     </StyledButton>
   )
