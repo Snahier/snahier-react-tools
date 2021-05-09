b13209fa10b97af083d6e64493bacc26ce6b7854
Snahier <hansalmeida@hotmail.com>
2021-05-09 05:05:32 -0300

    Remove unused components

--- src/pages/tooltip.tsx
+++ src/pages/tooltip.tsx
@@ -1,9 +1,7 @@
 import { PopperPortal } from "components/core/Popper/PopperPortal"
 import { PopperPortalWrapper } from "components/core/Popper/PopperPortalWrapper"
-import { PopperTooltip } from "components/core/Popper/PopperTooltip"
 import { useRef } from "react"
 import styled from "styled-components/macro"
-import { Tooltip } from "../components/core/Tooltip"
 import { Header } from "../components/templates/Header"
 
 interface PageTooltipProps {}
@@ -17,35 +15,6 @@ export default function PageTooltip({ ...props }: PageTooltipProps) {
       <h2>Tooltip Component</h2>
 
       <ButtonsContainer>
-        <Tooltip>
-          <button>hover me</button>
-        </Tooltip>
-
-        <Tooltip clickMode>
-          <button>click me</button>
-        </Tooltip>
-
-        <Tooltip clickMode arrow>
-          <button>tooltip with arrow</button>
-        </Tooltip>
-
-        <OverflowHiddenWrapper>
-          <PopperTooltip
-            arrow
-            content={
-              <div>
-                test test test
-                <br />
-                test test test
-                <br />
-                test test test
-              </div>
-            }
-          >
-            <button>tooltip</button>
-          </PopperTooltip>
-        </OverflowHiddenWrapper>
-
         <OverflowHiddenWrapper>
           <PopperPortal targetRef={buttonRef}>
             <CustomTooltip>Portal Popper</CustomTooltip>
