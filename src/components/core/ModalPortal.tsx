017e7b550815c54fe6ae260da53f9f9d3658ccd7
Snahier <hansalmeida@hotmail.com>
2021-05-09 08:09:01 -0300

    Add content rendering + dynamic zindex

--- src/components/core/ModalPortal.tsx
+++ src/components/core/ModalPortal.tsx
@@ -1,24 +1,70 @@
+import { useEffect, useState } from "react"
 import { createPortal } from "react-dom"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
+interface Position {
+  top?: string | number
+  bottom?: string | number
+  left?: string | number
+  right?: string | number
+  transform?: string
+}
 interface ModalPortalProps {
   isOpen: boolean
   onClose: any
+  content: React.ReactNode
+  position: Position
 }
 
 export const ModalPortal = ({
   isOpen,
   onClose,
+  content,
+  position,
   ...props
 }: ModalPortalProps) => {
-  return (
-    <StyledModalPortal {...props}>
-      {isOpen && createPortal(<Background onClick={onClose} />, document.body)}
-    </StyledModalPortal>
-  )
+  const [amountOfModals, setAmountOfModals] = useState(0)
+
+  useEffect(() => {
+    setAmountOfModals(
+      document.getElementsByClassName("component-ModalPortal").length
+    )
+  }, [isOpen])
+
+  return isOpen
+    ? createPortal(
+        <StyledModalPortal
+          {...props}
+          className="component-ModalPortal"
+          amountOfModals={amountOfModals}
+        >
+          <Background onClick={onClose} />
+          <Content
+            style={{
+              top: position.top,
+              bottom: position.bottom,
+              left: position.left,
+              right: position.right,
+              transform: position.transform,
+            }}
+          >
+            {content}
+          </Content>
+        </StyledModalPortal>,
+        document.body
+      )
+    : null
 }
 
-const StyledModalPortal = styled.div``
+type StyledModalPortalProps = {
+  amountOfModals: number
+}
+const StyledModalPortal = styled.div<StyledModalPortalProps>`
+  ${({ amountOfModals }) => css`
+    position: relative;
+    z-index: ${10 + amountOfModals};
+  `}
+`
 
 const Background = styled.div`
   position: fixed;
@@ -28,4 +74,10 @@ const Background = styled.div`
   right: 0;
 
   background-color: rgba(0, 0, 0, 0.5);
+`
+
+const Content = styled.div`
+  position: fixed !important;
+  top: 0;
+  left: 0;
 `
--- src/pages/modal-portal.tsx
+++ src/pages/modal-portal.tsx
@@ -5,6 +5,7 @@ import { Header } from "../components/templates/Header"
 
 export default function PageModalPortal({ ...props }) {
   const [isModal1Open, setIsModal1Open] = useState(false)
+  const [isModal2Open, setIsModal2Open] = useState(false)
 
   return (
     <StyledPageModalPortal {...props} className="page">
@@ -13,15 +14,45 @@ export default function PageModalPortal({ ...props }) {
 
       <Content>
         <button onClick={() => setIsModal1Open(true)}>Open modal 1</button>
-
-        <button>Open modal 2</button>
-
-        <button>Open modal 3</button>
+        <button onClick={() => setIsModal2Open(true)}>Open modal 2</button>
       </Content>
 
       <ModalPortal
         isOpen={isModal1Open}
         onClose={() => setIsModal1Open(false)}
+        position={{
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+        content={
+          <div
+            style={{
+              background: "red",
+              padding: "5rem",
+            }}
+          >
+            <button onClick={() => setIsModal2Open(true)}>Open modal 2</button>
+          </div>
+        }
+      />
+      <ModalPortal
+        isOpen={isModal2Open}
+        onClose={() => setIsModal2Open(false)}
+        position={{
+          top: 0,
+          left: 0,
+        }}
+        content={
+          <div
+            style={{
+              background: "blue",
+              padding: "5rem",
+            }}
+          >
+            <button onClick={() => setIsModal1Open(true)}>Open modal 1</button>
+          </div>
+        }
       />
     </StyledPageModalPortal>
   )
